ca5044777d787794dfb466e93fa4028e2dba0fb7
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-05-07 23:14:06 +0800

    Add files via upload

---
+++ contracts/Reward/BaseRewardEscrow.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Inheritance
+import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract BaseRewardEscrow is Ownable {
+    
+}
