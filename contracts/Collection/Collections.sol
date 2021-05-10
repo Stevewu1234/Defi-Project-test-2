e931a8cfe15326fa8570204ae27e15454ec56c13
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-05-10 22:29:43 +0800

    Add files via upload

--- contracts/Collection/Collections.sol
+++ contracts/Collection/Collections.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.6.0;
 import "../../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import "../../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
 import "../../node_modules/openzeppelin-solidity/contracts/utils/Counters.sol";
-import "../../node_modules/openzeppelin-solidity/contracts/proxy/Initializable.sol";
 
-contract NFT_test is ERC721,Ownable {
+contract Collection is ERC721,Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdTracker;
@@ -13,64 +12,67 @@ contract NFT_test is ERC721,Ownable {
     constructor(
         string memory _name,
         string memory _symbol,
-        string memory _baseUri
+        string memory _baseUri,
+        address owner
     ) public Ownable() ERC721(_name, _symbol) {
-        setBaseURI(_baseUri);
+        _setBaseURI(_baseUri);
+        transferOwnership(owner);
     }
 
     /** ===================== external mutative function ===================== */
 
     function mint(address to) external {
         _singlemint(to);
-        emit singlemint(to, _tokenIdTracker.current());
     }
 
     function batchmint(address to, uint amount) external {
         _batchmint(to,amount);
-        emit batchmint(to, amount);
-    } 
-
-    function changeBaseURI(string memory baseURI_) external {
-        _setBaseURI(baseURL_);
     }
 
-    function changetokenURI(uint256 tokenId, string memory tokenURI) external {
-        _setTokenURI(tokenId, tokenURI);
+    function changeBaseURI(string memory baseURI_) external onlyOwner {
+        _changeBaseURI(baseURI_);
     }
 
-    function initialize(
-        string memory name,
-        string memory symbol,
-        string memory baseUri,
-        address owner
-    ) external initializer() {
-        _name = name;
-        _symbol = symbol;
-        setBaseURI(_baseUri);
-        transferOwnership(owner);
+    function changetokenURI(uint256 tokenId, string memory tokenURI) external onlytokenOwner(tokenId) {
+        _changetokenURI(tokenId, tokenURI);
     }
+
 
     /** ===================== internal mutative function ===================== */
 
     function _batchmint(address to,uint amount) internal {
         require(amount != 0, "you must set a amount");
-        for(i=0; i<amount;i++){
+        for(uint i=0; i<amount;i++){
             _singlemint(to);
         }
+        emit batchmints(to, amount);
     }
 
-    function _singlemint(address to) external {
+    function _singlemint(address to) internal {
         require(to != address(0), "to address is not allowed be zero");
         _safeMint(to, _tokenIdTracker.current());
         _tokenIdTracker.increment();
     }
-
-    /** ===================== view ===================== */
-
-    /** ===================== event ===================== */ 
-    event singlemint(address to, uint tokenid);
-    event batchmint(address to, uint amount);
     
+    function _changeBaseURI(string memory baseURI_) internal {
+        _setBaseURI(baseURI_);
+    }
+    
+    function _changetokenURI(uint256 tokenId, string memory tokenURI) internal {
+        _setTokenURI(tokenId, tokenURI);
+        emit changedtokenURI(msg.sender, tokenId, tokenURI);
+    }
+
+    /** ===================== modifier ===================== */
+    modifier onlytokenOwner(uint tokenId) {
+        require(ownerOf(tokenId) == msg.sender, "Only the owner can modify the tokenURI");
+        _;
+    }
+    
+    /** ===================== event ===================== */ 
+    
+    event batchmints(address indexed to, uint indexed amount);
+    event changedtokenURI(address indexed owner, uint indexed tokenId, string indexed tokenURI);
 
     
 }
