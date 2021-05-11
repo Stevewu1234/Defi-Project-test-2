6fd38452744440c05c1989314ef5eba52cc0cce7
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-05-11 20:39:44 +0800

    Add files via upload

---
+++ contracts/Reward/RewardEscrow.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Inheritance
+import "./BaseRewardEscrow.sol";
+
+contract RewardEscrow is BaseRewardEscrow {
+    
+    constructor (address _resolver) BaseRewardEscrow(_resolver) {}
+
+    
+    /** ========== public mutative functions ========== */
+    
+    /** ========== public view functions ========== */
+    
+    /** ========== external mutative functions ========== */
+
+    /** ========== external view functions ========== */
+
+    /** ========== internal mutative functions ========== */
+
+    /** ========== internal view functions ========== */
+
+    /** ========== modifier ========== */
+
+    /** ========== event ========== */
+}
