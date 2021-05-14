f2dd298a693a2699e7f923ddcb4a9f219883a7af
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-05-14 22:59:12 +0800

    Add files via upload

--- contracts/State/RewardState.sol
+++ contracts/State/RewardState.sol
@@ -2,5 +2,48 @@
 pragma solidity ^0.8.0;
 
 contract RewardState {
+
+
+    uint public TotalRewardsToDistritute;
+
+    uint public TotalRewardsClaimed;
+
+    uint public timelock;
+
+
+    // The share of RewardPool
+    uint public LiquidityShare;
+
+    uint public TradingShare;
+
+
+
+    /** ========== public mutative functions ========== */
     
+
+
+    /** ========== public view functions ========== */
+    
+
+
+
+    /** ========== external mutative functions ========== */
+
+
+    /** ========== external view functions ========== */
+    function getTotalRewardsToDistribute()
+
+
+    /** ========== internal mutative functions ========== */
+
+
+
+    /** ========== internal view functions ========== */
+
+    /** ========== private mutative functions ========== */
+
+
+    /** ========== modifier ========== */
+
+    /** ========== event ========== */
 }
