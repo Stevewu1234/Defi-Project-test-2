7687e2c3039eeec0256a373103ba7d6ffcbaeda9
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-06-22 22:25:47 +0800

    Add files via upload

--- contracts/Portal.sol
+++ contracts/Portal.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 // Inheritance
-import "./Tools/CacheResolver.sol";
+import "./Tools/CacheResolverUpgradeable.sol";
 
 // Internal References
 import "./Interface/IToken.sol";
@@ -10,28 +10,28 @@ import "./Interface/ISystemStatus.sol";
 import "./Interface/IRewardState.sol";
 import "./Interface/IRewardEscrowUpgradeable.sol";
 
-contract Portal is OwnableUpgradeable,CacheResolver {
+contract Portal is OwnableUpgradeable,CacheResolverUpgradeable {
     
 
     /* ========== Address Resolver configuration ==========*/
-    bytes32 private constant CONTRACT_STAKETOKEN = "StakeToken";
+    bytes32 private constant CONTRACT_TOKEN = "Token";
     bytes32 private constant CONTRACT_SYSTEMSTATUS = "SystemStatus";
     bytes32 private constant CONTRACT_REWARDSTATE = "RewardState";
-    bytes32 private constant CONTRACT_REWARDESCROW = "RewardEscrow";
+    bytes32 private constant CONTRACT_REWARDESCROWUPGRADEABLE = "RewardEscrowUpgradeable";
 
-    function resolverAddressesRequired() public view returns (bytes32[] memory addresses) {
+    function resolverAddressesRequired() public view override returns (bytes32[] memory) {
         bytes32[] memory addresses = new bytes32[](4);
-        addresses[0] = CONTRACT_STAKETOKEN;
+        addresses[0] = CONTRACT_TOKEN;
         addresses[1] = CONTRACT_SYSTEMSTATUS;
         addresses[2] = CONTRACT_REWARDSTATE;
-        addresses[3] = CONTRACT_REWARDESCROW;
+        addresses[3] = CONTRACT_REWARDESCROWUPGRADEABLE;
         return addresses;
     }
 
-    function stakeToken() internal view returns (IToken) {
-        return IToken(requireAndGetAddress(CONTRACT_STAKETOKEN));
+    function token() internal view returns (IToken) {
+        return IToken(requireAndGetAddress(CONTRACT_TOKEN));
     }
-    function systemStatue() internal view returns (ISystemStatus) {
+    function systemStatus() internal view returns (ISystemStatus) {
         return ISystemStatus(requireAndGetAddress(CONTRACT_SYSTEMSTATUS));
     }
 
@@ -39,12 +39,12 @@ contract Portal is OwnableUpgradeable,CacheResolver {
         return IRewardState(requireAndGetAddress(CONTRACT_REWARDSTATE));
     }
         
-    function rewardEscrow() internal view returns (IRewardEscrowUpgradeable) {
-        return IRewardEscrowUpgradeable(requireAndGetAddress(CONTRACT_REWARDESCROW));
+    function rewardEscrowUpgradeable() internal view returns (IRewardEscrowUpgradeable) {
+        return IRewardEscrowUpgradeable(requireAndGetAddress(CONTRACT_REWARDESCROWUPGRADEABLE));
     }
  
 
-    /** ========== variables ========== */
+    /* ========== variables ========== */
     uint internal totalLockedAmount;
     
     mapping (address => uint) internal accountBalancesLockedAmount;
@@ -53,10 +53,10 @@ contract Portal is OwnableUpgradeable,CacheResolver {
 
     mapping (address => uint) internal accountEscrowedAndAvailableAmount;
 
-    function portal_init(address _resolver) initializer {
+    function portal_init(address _resolver) external initializer {
         __Ownable_init();
         _cacheInit(_resolver);
-        EscrowedAndAvailableAmount[account] = 0;
+        accountEscrowedAndAvailableAmount[_msgSender()] = 0;
     }
 
     /** ========== public view functions ========== */
@@ -77,8 +77,8 @@ contract Portal is OwnableUpgradeable,CacheResolver {
     }
 
     function getAccountTotalLockedAmount(address account) public view returns (uint accountTotalLockedAmount) {
-        balancesLockedAmount = getAccountBalancesLockedAmount(account);
-        escrowedLockedAmount = getAccountEscrowedLockedAmount(account);
+        uint balancesLockedAmount = getAccountBalancesLockedAmount(account);
+        uint escrowedLockedAmount = getAccountEscrowedLockedAmount(account);
         accountTotalLockedAmount = balancesLockedAmount + escrowedLockedAmount;
 
         return accountTotalLockedAmount;
@@ -87,24 +87,24 @@ contract Portal is OwnableUpgradeable,CacheResolver {
 
     /** ========== public mutative functions ========== */
 
-    /**
+    /*
      * @description: stake value to get an attendace rate and a NFT.
      * @dev update user's attendace rate which will be save in the state contract.
      * @param value the amount to stake
      */
-    function enter(address player, uint value, bool enterall) public enterActive {
+    function enter(address player, uint value, bool enterall) public {
         // get user's available token to enter the ecosystem including the escrowed amount
-        (accountTotalAvailableAmount, balanceOfuser, balanceOfEscrowed) = _remaininigAvailaleAmount(player);
+        (uint accountTotalAvailableAmount, uint balanceOfuser, uint balanceOfEscrowed) = _remaininigAvailaleAmount(player);
 
-        require(value < accountTotalAvailableAmount, "you don't have enough token to enter");
+        require(value <= accountTotalAvailableAmount, "you don't have enough token to enter");
 
         // register user's token and lock registered amount
-        _registerPortalLock(balanceOfuser, balanceOfEscrowed, value, enterall);
+        _registerPortalLock(player, balanceOfuser, balanceOfEscrowed, value, enterall);
 
         emit entered(player, value, enterall);
     }
 
-    /**
+    /*
      * @description: withdraw value which users enter with.
      * @dev update user's attendace rate and 
      * @param {*}
@@ -112,19 +112,19 @@ contract Portal is OwnableUpgradeable,CacheResolver {
     function withdrow(address player, uint value, bool withdrawall) public {
 
         // get user's locked token to withdraw
-        (balancesLockedAmount, escrowedLockedAmount) = _getAccountLockedAmount(player);
+        (uint balancesLockedAmount, uint escrowedLockedAmount) = _getAccountLockedAmount(player);
 
         // remove locked token register
-        _removeRegisterPortalLock(balancesLockedAmount, escrowedLockedAmount, value, withdrawall);
+        _removeRegisterPortalLock(player, balancesLockedAmount, escrowedLockedAmount, value, withdrawall);
 
         emit withdrawn(player, value, withdrawall);
     }
 
-    function getReward(address player, uint _rewardSaveDuration) public getRewardActive {
+    function getReward(address player, uint _rewardSaveDuration) public {
         rewardState().getReward(player, _rewardSaveDuration);
     }
 
-    function exit(address player, uint value, bool withdrawall, uint _rewardSaveDuration) external {
+    function exit(address player, uint value, bool withdrawall, uint _rewardSaveDuration) public {
         withdrow(player, value, withdrawall);
         getReward(player, _rewardSaveDuration);
     }
@@ -138,25 +138,41 @@ contract Portal is OwnableUpgradeable,CacheResolver {
         emit transferredEscrowedToBalancesLocked(account, amount);
     }
 
+    // update account escrowed token available quota
+    function updateAccountEscrowedAndAvailableAmount(address account, uint amount, bool add, bool sub) external onlyRewardEscrow {
+        require(add != sub, "not allowed to be the same");
+        
+
+        if(add == true) {
+            accountEscrowedAndAvailableAmount[account] = accountEscrowedAndAvailableAmount[account] + amount;
+        }
+
+        if(sub == true) {
+            require(accountEscrowedAndAvailableAmount[account] >= amount, "you don't have enough escrowed token");
+            accountEscrowedAndAvailableAmount[account] = accountEscrowedAndAvailableAmount[account] - amount;
+        }
+    }
 
     /** ========== external view function ========== */
 
-    function getTransferableAmount(address account, uint value) external view returns (uint transferable) {
+    function getTransferableAmount(address account) external view returns (uint transferable) {
 
         // transferable token will be only calculated from balances of user, excluding escrowed token,
         // becasue escrowed token is not allowed to transfer between wallet.
-        uint acountlockedamount = accountBalancesLockedAmount[accunt];
+        uint acountlockedamount = accountBalancesLockedAmount[account];
+        uint balanceOf = token().balanceOf(account);
 
-        if( value <= acountlockedamount) {
+        if( balanceOf <= acountlockedamount) {
             transferable = 0;
         } else {
-            transferable = value - acountlockedamount;
+            transferable = balanceOf - acountlockedamount;
         }
     }
 
     /** ========== internal mutative functions ========== */
 
     function _registerPortalLock(
+        address player,
         uint balanceOfuser,
         uint balanceOfEscrowed,
         uint value,
@@ -164,26 +180,28 @@ contract Portal is OwnableUpgradeable,CacheResolver {
     ) internal {
         
         if(enterall == true) {
-            _updateAccountBalancesLockedAmount(_msgSender(), balanceOfuser, true, false);
-            _updateAccountEscrowedLockedAmount(_mesSender(), balanceOfEscrowed, true, false);
+            _updateAccountBalancesLockedAmount(player, balanceOfuser, true, false);
+            _updateAccountEscrowedLockedAmount(player, balanceOfEscrowed, true, false);
+            
         }
         
         // if user doesn't enter all token, system will preferentially enter all escorwed available token
         if(enterall == false) {
-            if(value > balanceOfEscrowed) {
+            if(value >= balanceOfEscrowed) {
                 uint _resttoken = value - balanceOfEscrowed;
-                _updateAccountEscrowedLockedAmount(_mesSender(), balanceOfEscrowed, true, false);
-                _updateAccountBalancesLockedAmount(_msgSender(), _resttoken, true, false);
+                _updateAccountEscrowedLockedAmount(player, balanceOfEscrowed, true, false);
+                _updateAccountBalancesLockedAmount(player, _resttoken, true, false);
             }
 
             if(value < balanceOfEscrowed) {
-                _updateAccountEscrowedLockedAmount(_mesSender(), value, true, false);
+                _updateAccountEscrowedLockedAmount(player, value, true, false);
             }
         }
 
     }
 
     function _removeRegisterPortalLock(
+        address player,
         uint balancesLockedAmount, 
         uint escrowedLockedAmount, 
         uint value, 
@@ -191,40 +209,25 @@ contract Portal is OwnableUpgradeable,CacheResolver {
         ) internal {
 
             if(withdrawall == true) {
-                _updateAccountBalancesLockedAmount(_msgSender(), balancesLockedAmount, false, true);
-                _updateAccountEscrowedLockedAmount(_msgSender(), escrowedLockedAmount, false, true);
+                _updateAccountBalancesLockedAmount(player, balancesLockedAmount, false, true);
+                _updateAccountEscrowedLockedAmount(player, escrowedLockedAmount, false, true);
             }
 
             if(withdrawall == false) {
                 if(value > escrowedLockedAmount) {
                     uint _resttoken = value - escrowedLockedAmount;
-                    _updateAccountEscrowedLockedAmount(_msgSender(), escrowedLockedAmount, false, true);
-                    _updateAccountBalancesLockedAmount(_msgSender(), balancesLockedAmount, false, true);
+                    _updateAccountEscrowedLockedAmount(player, escrowedLockedAmount, false, true);
+                    _updateAccountBalancesLockedAmount(player, _resttoken, false, true);
                 }
 
                 if(value < escrowedLockedAmount) {
-                    _updateAccountEscrowedLockedAmount(_msgSender(), value, false, true);
+                    _updateAccountEscrowedLockedAmount(player, value, false, true);
                 }
             }
     }
 
-    // update account escrowed token available quota
-    function _updateAccountEscrowedAndAvailableAmount(address account, uint amount, bool add, bool sub) internal {
-        require(add != sub, "not allowed to be the same");
-        
-
-        if(add == true) {
-            accountEscrowedAndAvailableAmount[account] = EscrowedAndAvailableAmount[account] + amount;
-        }
-
-        if(sub == true) {
-            require(accountEscrowedAndAvailableAmount[account] >= amount, "you don't have enough escrowed token");
-            accountEscrowedAndAvailableAmount[account] = EscrowedAndAvailableAmount[account] - amount;
-        }
-    }
-
     // update account escrowed and locked token
-    function _updateAccountEscrowedLockedAmount(address account, updatingLockedAmount, bool add, bool sub) internal {
+    function _updateAccountEscrowedLockedAmount(address account, uint updatingLockedAmount, bool add, bool sub) internal {
         require(add != sub, "not allowed to be the same");
 
         if(add == true) {
@@ -245,19 +248,19 @@ contract Portal is OwnableUpgradeable,CacheResolver {
     } 
 
     // update account balances locked token
-    function _updateAccountBalancesLockedAmount(address account, uint updatingLockAmount, bool add, bool sub) internal {
+    function _updateAccountBalancesLockedAmount(address account, uint updatingLockedAmount, bool add, bool sub) internal {
         require(add != sub, "not allowed to be the same");
 
         if(add == true) {
-            accountBalancesLockedAmount[account] += updatingLockAmount;
+            accountBalancesLockedAmount[account] += updatingLockedAmount;
 
-            totalLockedAmount += updatingLockAmount;
+            totalLockedAmount += updatingLockedAmount;
         }
 
         if(sub == true) {
-            accountBalancesLockedAmount[account] -= updatingLockAmount;
+            accountBalancesLockedAmount[account] -= updatingLockedAmount;
 
-            totalLockedAmount -= updatingLockAmount;
+            totalLockedAmount -= updatingLockedAmount;
         }
     }
 
@@ -268,10 +271,10 @@ contract Portal is OwnableUpgradeable,CacheResolver {
         uint balanceOfuser,
         uint balanceOfEscrowed
     ) {
-        balanceOfuser = stakeToken().balanceOf(account);
-        balanceOfEscrowed = balanceOfEscrowedAndAvailableAmount(account);
-        totalBalanceOfuser = balanceOfuser + balanceOfEscrowed;
-        return (totalBalanceOfuser, totalBalanceOf, balanceOfEscrowed);
+        balanceOfuser = token().balanceOf(account);
+        balanceOfEscrowed = accountEscrowedAndAvailableAmount[account];
+        accountTotalAvailableAmount = balanceOfuser + balanceOfEscrowed;
+        return (accountTotalAvailableAmount, balanceOfuser, balanceOfEscrowed);
     }
 
     function _getAccountLockedAmount(address account) internal view returns (
@@ -286,17 +289,7 @@ contract Portal is OwnableUpgradeable,CacheResolver {
 
     /** ========== modifier ========== */
     modifier onlyRewardEscrow() {
-        require(address(rewardEscrow()) == _msgSender(), "only rewardEscrow contract can access");
-        _;
-    }
-
-    modifier enterActive() {
-        require(systemStatus().requireFunctionActive(bytes32("enter"), bytes32("System")), "the function of enter is inactive");
-        _;
-    }
-
-    modifier getRewardActive() {
-        require(systemStatus().requireFunctionActive(bytes32("getreward"), bytes32("System")), "the function of getreward is inactive");
+        require(address(rewardEscrowUpgradeable()) == _msgSender(), "only rewardEscrow contract can access");
         _;
     }
 
